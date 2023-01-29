a99504fe782bbcdaab5da5c759b7aea6c1d1f7be
darkmyter <dammak.bader@gmail.com>
2023-01-29 10:27:51 +0100

    adds eval step to train

--- Computer-vision/object-detection/yolov1/train.py
+++ Computer-vision/object-detection/yolov1/train.py
@@ -5,10 +5,12 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 from model import Yolov1
 from pathlib import Path
-from dataset import get_VOCDataset
+from dataset import get_VOCDataset, label_to_list
 from utils import (
     save_checkpoint,
     load_checkpoint,
+    non_max_suppression,
+    mean_average_precision
 )
 from loss import YoloLoss
 
@@ -33,6 +35,10 @@ class WrappedChainScheduler(torch.optim.lr_scheduler.ChainedScheduler):
 
 
 def train(device, model, optimizer, loss_function, train_loader, test_loader, scheduler, params, model_name):
+
+    iou_threshold = 0.5
+    threshold = 0.4
+
     for epoch in range(params['epochs']):
         loop = tqdm(train_loader, leave=True)
 
@@ -56,8 +62,41 @@ def train(device, model, optimizer, loss_function, train_loader, test_loader, sc
             save_checkpoint(model, optimizer, scheduler, model_name)
 
         if params["test"] and epoch % params['num_epochs_between_tests'] == 0:
-            pass
-            # TODO
+            model.eval()
+            with torch.no_grad():
+                pred_bboxs = []
+                labels_bboxs = []
+                train_idx = 0
+                for x, y in train_loader:
+                    x, y = x.to(device), y.to(device)
+                    pred = model(x)
+                    pred = pred.reshape(pred.shape[0], 7, 7, -1)
+
+                    for b_idx in range(pred.shape[0]):
+
+                        true_bboxs = label_to_list(y[b_idx, ...], threshold=0)
+                        bboxes = label_to_list(pred[b_idx, ...], threshold=0)
+
+                        nms_boxes = non_max_suppression(
+                            bboxes,
+                            iou_threshold=iou_threshold,
+                            threshold=threshold,
+                        )
+                        for nms_box in nms_boxes:
+                            pred_bboxs.append([train_idx] + nms_box)
+
+                        for box in true_bboxs:
+                            if box[1] > threshold:
+                                labels_bboxs.append([train_idx] + box)
+                        train_idx += 1
+
+                mean_avg_prec = mean_average_precision(
+                    pred_bboxs, labels_bboxs, iou_threshold=iou_threshold
+                )
+
+            print(f"Test mAP: {mean_avg_prec:.3f}")
+            
+            model.train()
 
     save_checkpoint(model, optimizer, scheduler, model_name)
 
@@ -132,10 +171,10 @@ def main(model_name, new, seed):
         "pin_memory": True,
         "img_dir": Path("../../../data/voc/images"),
         "label_dir": Path("../../../data/voc/labels"),
-        "training_csv": Path("../../../data/voc/train.csv"),
+        "training_csv": Path("../../../data/voc/100examples.csv"),
         "test_csv": Path("../../../data/voc/test.csv"),
         "num_epochs_between_checkpoints": 5,
-        "num_epochs_between_tests": 10,
+        "num_epochs_between_tests": 1,
         "test": True
     }
 
